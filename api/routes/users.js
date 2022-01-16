c9bb579dba3895c09fe08cd4d1831dad0f86554d
m-swelam <mid2o0o11@gmail.com>
2022-01-17 00:32:08 +0200

    get a user route

--- api/routes/users.js
+++ api/routes/users.js
@@ -42,4 +42,16 @@ router.delete('/:id', async (req, res) => {
 	}
 })
 
+// get a user
+router.get('/:id', async (req, res) => {
+	try {
+		const user = await User.findById(req.params.id)
+		const { isAdmin, password, updatedAt, ...other } = user._doc
+
+		res.status(200).json(other)
+	} catch (err) {
+		res.status(500).json(err)
+	}
+})
+
 module.exports = router
