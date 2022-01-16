f2fb013790dca5d7261da099f50001f0a57c5542
m-swelam <mid2o0o11@gmail.com>
2022-01-16 21:16:22 +0200

    post first user api

--- api/models/User.js
+++ api/models/User.js
@@ -1,42 +1,47 @@
 const mongoose = require('mongoose')
 
-const UserShema = new mongoose.Schema({
-	userName: {
-		type: String,
-		required: true,
-		min: 3,
-		max: 20,
-		unique: true,
+const UserShema = new mongoose.Schema(
+	{
+		userName: {
+			type: String,
+			required: true,
+			min: 3,
+			max: 20,
+			unique: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			max: 50,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+			min: 6,
+		},
+		profilePicture: {
+			type: String,
+			default: '',
+		},
+		coverPicture: {
+			type: String,
+			default: '',
+		},
+		followres: {
+			type: Array,
+			default: [],
+		},
+		followings: {
+			type: Array,
+			default: [],
+		},
+		isAdmin: {
+			type: Boolean,
+			default: false,
+		},
 	},
-	email: {
-		type: String,
-		required: true,
-		max: 50,
-		unique: true,
-	},
-	password: {
-		type: String,
-		required: true,
-		min: 6,
-	},
-	profilePicture: {
-		type: String,
-		default: '',
-	},
-	coverPicture: {
-		type: String,
-		default: '',
-	},
-	followres: {
-		type: Array,
-		default: [],
-	},
-	followings: {
-		type: Array,
-		default: [],
-	},
-	isAdmin: {
-		type: Boolean,
-		default: false,
-	},
-})
+	{ timestamps: true }
+)
+
+module.exports = mongoose.model('User', UserShema)
