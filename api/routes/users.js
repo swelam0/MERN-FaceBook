4c4267157db7d42de1d6465e84775dd599eba632
m-swelam <mid2o0o11@gmail.com>
2022-01-17 00:23:32 +0200

    update and delete user route

--- api/routes/users.js
+++ api/routes/users.js
@@ -8,6 +8,7 @@ router.put('/:id', async (req, res) => {
 		if (req.body.password) {
 			try {
 				const salt = await bcrypt.genSalt(10)
+
 				req.body.password = await bcrypt.hash(req.body.password, salt)
 			} catch (err) {
 				res.status(500).json(err)
@@ -32,7 +33,6 @@ router.delete('/:id', async (req, res) => {
 	if (req.body.userId === req.params.id || req.body.isAdmin) {
 		try {
 			const user = await User.findByIdAndDelete(req.params.id)
-
 			res.status(200).json('Account has been deleted')
 		} catch (err) {
 			res.status(404).json(err)
