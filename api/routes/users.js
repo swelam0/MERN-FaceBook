2ffec1368a14bce48a1b543bbfdf7160400e78b3
m-swelam <mid2o0o11@gmail.com>
2022-01-17 00:22:20 +0200

    login user route

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,7 +1,45 @@
 const router = require('express').Router()
+const User = require('../models/User')
+const bcrypt = require('bcrypt')
 
-router.get('/', (req, res) => {
-	res.send('hey its user route')
+// update user
+router.put('/:id', async (req, res) => {
+	if (req.body.userId === req.params.id || req.User.isAdmin) {
+		if (req.body.password) {
+			try {
+				const salt = await bcrypt.genSalt(10)
+				req.body.password = await bcrypt.hash(req.body.password, salt)
+			} catch (err) {
+				res.status(500).json(err)
+			}
+		}
+
+		try {
+			const user = await User.findByIdAndUpdate(req.params.id, {
+				$set: req.body,
+			})
+			res.status(200).json('Account has been updated')
+		} catch (err) {
+			res.status(500).json(err)
+		}
+	} else {
+		return res.status(403).json('you can update only your account')
+	}
+})
+
+// delete user account
+router.delete('/:id', async (req, res) => {
+	if (req.body.userId === req.params.id || req.body.isAdmin) {
+		try {
+			const user = await User.findByIdAndDelete(req.params.id)
+
+			res.status(200).json('Account has been deleted')
+		} catch (err) {
+			res.status(404).json(err)
+		}
+	} else {
+		res.status(500).json('you only can delete your account')
+	}
 })
 
 module.exports = router
