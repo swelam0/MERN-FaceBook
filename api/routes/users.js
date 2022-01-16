d13022bb0470e64a2316f5c8469653a7023e9f68
m-swelam <mid2o0o11@gmail.com>
2022-01-17 01:53:07 +0200

    follow a user route

--- api/routes/users.js
+++ api/routes/users.js
@@ -54,4 +54,27 @@ router.get('/:id', async (req, res) => {
 	}
 })
 
+// follow a user
+router.post('/:id/follow', async (req, res) => {
+	if (req.body.userId !== req.params.id) {
+		try {
+			const user = await User.findById(req.params.id)
+			const currentUser = await User.findById(req.body.userId)
+
+			if (!user.followres.includes(req.params.id)) {
+				await user.updateOne({ $push: { followres: req.params.id } })
+				await currentUser.updateOne({ $push: { followings: req.body.userId } })
+
+				res.status(200).json('user has been followed')
+			} else {
+				res.status(403).json('you allready follow this user')
+			}
+		} catch (err) {
+			res.status(500).json(err)
+		}
+	} else {
+		res.status(403).json("You can't follow your self")
+	}
+})
+
 module.exports = router
