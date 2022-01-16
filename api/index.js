65a35467c6fe38abf70478afb3e72dd5edee0621
m-swelam <mid2o0o11@gmail.com>
2022-01-16 20:56:35 +0200

    user model

--- api/index.js
+++ api/index.js
@@ -3,9 +3,17 @@ const dotenv = require('dotenv')
 const mongoose = require('mongoose')
 const helmet = require('helmet')
 const morgan = require('morgan')
-
 const app = express()
 dotenv.config()
+
+// api routes
+const userRoute = require('./routes/users')
+const authRoute = require('./routes/auth')
+
+// express middleware
+app.use(express.json())
+app.use(helmet())
+app.use(morgan('common'))
 
 // mongoDB Connection
 main().catch((err) => console.log(err))
@@ -15,10 +23,16 @@ async function main() {
 	})
 }
 
-// express middleware
-app.use(express.json())
-app.use(helmet())
-app.use(morgan('common'))
+app.get('/', (req, res) => {
+	res.send('welcome to home page')
+})
+
+app.get('/users', (req, res) => {
+	res.send('welcome to user page')
+})
+
+app.use('/api/user', userRoute)
+app.use('/api/auth', authRoute)
 
 // express sever
 app.listen(8800, () => {
