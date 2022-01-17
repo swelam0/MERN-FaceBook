f323db809fb14354c83a8a14ab6007c6166e42f0
m-swelam <mid2o0o11@gmail.com>
2022-01-17 15:06:44 +0200

    home feed share component

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,11 +1,19 @@
 import styled from 'styled-components'
+import Share from '../share/Share'
 
 const Feed = () => {
-	return <Container>Feed</Container>
+	return (
+		<Container>
+			<div className='feedWrapper'>
+				<Share />
+			</div>
+		</Container>
+	)
 }
 const Container = styled.div`
 	flex: 6;
-	height: calc(100vh-50px);
+	height: calc(100vh - 50px);
+	padding: 0 20px;
 `
 
 export default Feed
---
+++ client/src/components/share/Share.jsx
@@ -0,0 +1,134 @@
+import styled from 'styled-components'
+import { PermMedia, Label, Room, EmojiEmotions } from '@material-ui/icons'
+
+const Share = () => {
+	return (
+		<Container>
+			<div className='shareWrapper'>
+				<div className='shareTop'>
+					<img src='assets/person/1.jpeg' alt='user' />
+					<input type='text' placeholder="what's in your mind?" />
+				</div>
+
+				<div className='shareBottom'>
+					<ul className='shareOptions'>
+						<li>
+							<PermMedia />
+							<span>Photo or Video</span>
+						</li>
+						<li>
+							<Label />
+							<span>Tag</span>
+						</li>
+						<li>
+							<Room />
+							<span>Location</span>
+						</li>
+						<li>
+							<EmojiEmotions />
+							<span>Feelings</span>
+						</li>
+					</ul>
+
+					<button className='shareButton'>Share</button>
+				</div>
+			</div>
+		</Container>
+	)
+}
+const Container = styled.div`
+	width: 100%;
+	border-radius: 10px;
+	overflow: hidden;
+	-webkit-box-shadow: 0px 0px 16px -8px rgba(0, 0, 0, 0.68);
+	box-shadow: 0px 0px 16px -8px rgba(0, 0, 0, 0.68);
+	padding-bottom: 10px;
+
+	.shareWrapper {
+		padding: 10px;
+	}
+
+	.shareTop {
+		display: flex;
+		align-items: center;
+		padding-bottom: 10px;
+		margin-bottom: 20px;
+		border-bottom: 1px solid var(--gray);
+		overflow: hidden;
+
+		input {
+			border: none;
+			width: 100%;
+
+			&:focus {
+				outline: none;
+			}
+		}
+
+		img {
+			width: 50px;
+			height: 50px;
+			border-radius: 50%;
+			object-fit: cover;
+			margin-right: 10px;
+		}
+	}
+
+	.shareBottom {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+
+		ul {
+			display: flex;
+			margin-left: 20px;
+			width: 100%;
+			justify-content: space-evenly;
+
+			li:nth-child(1) svg {
+				color: red;
+			}
+			li:nth-child(2) svg {
+				color: var(--blue);
+			}
+			li:nth-child(3) svg {
+				color: #116810;
+			}
+			li:nth-child(4) svg {
+				color: #212121;
+			}
+		}
+
+		li {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			margin-right: 15px;
+			cursor: pointer;
+
+			svg {
+				font-size: 18px;
+				margin-right: 3px;
+			}
+
+			span {
+				font-size: 14px;
+				font-weight: 500;
+			}
+		}
+	}
+
+	.shareButton {
+		border: none;
+		padding: 7px;
+		border-radius: 5px;
+		background-color: #025f02;
+		font-weight: 500;
+		margin-right: 20px;
+		width: 120px;
+		cursor: pointer;
+		color: white;
+	}
+`
+
+export default Share
