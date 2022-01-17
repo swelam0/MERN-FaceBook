a0a3f1be742a7221c5a5d2758d46ad7adbc17896
m-swelam <mid2o0o11@gmail.com>
2022-01-17 03:21:01 +0200

    update post api

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -20,6 +20,21 @@ router.post('/', async (req, res) => {
 })
 
 // update a post
+router.put('/:id', async (req, res) => {
+	const post = await Post.findById(req.params.id)
+
+	try {
+		if (post.userId === req.body.userId) {
+			await post.updateOne({ $set: req.body })
+			res.status(200).json('the post has been updated')
+		} else {
+			res.status(403).json('you can only update your posts')
+		}
+	} catch (err) {
+		res.status(500).json(err)
+	}
+})
+
 // delete a post
 // like a post
 //  get a post
