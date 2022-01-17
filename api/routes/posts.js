ac36d0506de105c54c3d6c3b5d5734e28a1bae55
m-swelam <mid2o0o11@gmail.com>
2022-01-17 03:57:49 +0200

    like post api

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -50,7 +50,28 @@ router.delete('/:id', async (req, res) => {
 		res.status(500).json(err)
 	}
 })
+
 // like a post
+router.put('/:id/like', async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id)
+		if (post) {
+			if (!post.likes.includes(req.body.userId)) {
+				await post.updateOne({ $push: { likes: req.body.userId } })
+
+				res.status(200).json('this post has been liked')
+			} else {
+				await post.updateOne({ $pull: { likes: req.body.userId } })
+
+				res.status(200).json('this post has been disliked')
+			}
+		} else {
+			res.status(404).json('post not found')
+		}
+	} catch (err) {
+		res.status(500).json(err)
+	}
+})
 //  get a post
 router.get('/:id', async (req, res) => {
 	try {
